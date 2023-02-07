d2fafed75796d02d310ca71d1e536c64370abaa4
AbigailSc <abigailsarzuri@gmail.com>
2023-02-07 00:30:06 -0300

    style: characters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharactersComponent } from './components/characters/characters.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, CharactersComponent, NavbarComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/servicios/api.service.ts
+++ src/app/servicios/api.service.ts
@@ -8,15 +8,17 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   // public idCharacter: string = '5f8d3a7f-467b-97f3-062c-13acf203c006';
-  private url = 'https://valorant-api.com/v1/agents?isPlayableCharacter=true';
+  private url = 'https://valorant-api.com/v1/';
 
   constructor(private http: HttpClient) {}
 
   public getCharacters(): Observable<Characters> {
-    return this.http.get<Characters>(this.url);
+    return this.http.get<Characters>(
+      `${this.url}agents?isPlayableCharacter=true`
+    );
   }
 
   public getCharacterById(idCharacter: string): Observable<Characters> {
-    return this.http.get<Characters>(`${this.url}/${idCharacter}`);
+    return this.http.get<Characters>(`${this.url}/agents/${idCharacter}`);
   }
 }
